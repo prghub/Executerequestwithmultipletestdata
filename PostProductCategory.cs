fd840730e795be7ecd1a5b050ea624ebfc03244e
prghub <51963993+prghub@users.noreply.github.com>
2021-05-04 10:45:26 +0100

    Add files via upload

---
+++ PostProductCategory.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductService.Models.Request;
+using RestSharp;
+using System;
+using ProductService.Models.Request;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ProductService.TestProductCategory.Tests
+{
+    /// <summary>
+    /// Tests for the endpoints in ProductService
+    /// https://cascadetest-productservice.azurewebsites.net/swagger/index.html
+    /// </summary>
+    [TestClass]
+    public class PostProductCategory : ProductServiceTest
+    {
+        /// <summary>
+        /// Post Request for create product category
+        /// Tests the status code, response body and schema
+        /// </summary>
+        [TestMethod]
+        [TestCategory("ProductCategory")]
+        public void TestCreateProductCategory()
+        {
+            System.Diagnostics.Debug.WriteLine("ProductCategory");
+
+            string path = @"C:\Users\preethy.ramamoorthy\source\repos\cascade-api-test-framework\ProductService\ProductCategory\TestData1.json";
+            string readText = File.ReadAllText(path);
+            restRequest.Resource = "productcategories";
+            restRequest.Method = Method.POST;
+            //restRequest.AddParameter("application/json", "C:/Users/preethy.ramamoorthy/source/repos/cascade-api-test-framework/ProductService/ProductCategory/TestData2.json", ParameterType.RequestBody);
+            restRequest.AddJsonBody(readText);
+
+            IRestResponse response = restClient.Execute(restRequest);
+            System.Diagnostics.Debug.WriteLine("1.." + response.Content);
+            System.Diagnostics.Debug.WriteLine("2.." + response.StatusCode);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        }
+
+        /// <summary>
+        /// Post Request for create product category
+        /// Tests the status code, response body and schema
+        /// </summary>
+        [TestMethod]
+        [TestCategory("ProductCategory")]
+        public void TestCreateProductCategoryFakeData()
+        {
+            restRequest.Resource = "productcategories";
+            restRequest.Method = Method.POST;
+
+            List<ProductCategory> lstProdCat = CreateProductCategory(100);
+
+            List<IRestResponse> responseList = new List<IRestResponse>();
+
+            foreach (ProductCategory prodCat in lstProdCat)
+            {
+                restRequest.AddJsonBody(prodCat);
+                System.Diagnostics.Debug.WriteLine(JsonConvert.SerializeObject(prodCat));
+
+
+                IRestResponse response = restClient.Execute(restRequest);
+                responseList.Add(response);
+            }
+
+            foreach (IRestResponse response in responseList)
+            {
+                System.Diagnostics.Debug.WriteLine(response.StatusCode + "----" + response.Content);
+
+            }
+
+        }
+
+
+        /// <summary>
+        /// Post Request for create product category
+        /// Tests the status code, response body and schema
+        /// </summary>
+        [TestMethod]
+        [TestCategory("ProductCategory")]
+        public void TestCreateProductCategoryValid()
+        {
+            restRequest.Resource = "productcategories";
+            restRequest.Method = Method.POST;
+
+            Audit audit = new Audit(1, 130, null, null, false);
+
+            ProductCategory prodCat = new ProductCategory("PRTEST" + Faker.Lorem.Sentence(1), Faker.Lorem.Sentence(5), 11, false, null, audit);
+
+            // Add OrderDTO to Rest Request
+            restRequest.AddJsonBody(prodCat);
+
+            //Exucute the API
+            IRestResponse response = restClient.Execute(restRequest);
+
+            String responseString = response.Content;
+
+            System.Diagnostics.Debug.WriteLine("Response body is-------" + responseString);
+
+            //To test the response status
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        }
+    }
+}
---
+++ ServiceTest.cs
@@ -0,0 +1,122 @@
+﻿using Cascade_Functional_Tests.Authentication;
+using Cascade_Functional_Tests.Helper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductService.Models.Request;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Cascade_Functional_Tests.Base
+{
+    [TestClass]
+    public class ServiceTest
+    {
+        protected RestClient restClient;
+        protected String access;
+        protected RestRequest restRequest;
+        protected IRestResponse response;
+
+        protected String scope;
+        protected String url;
+
+        protected IConfigurationRoot configuration;
+
+        public ServiceTest()
+        {
+            configuration = new ConfigurationBuilder()
+           .AddJsonFile("ConfigProperties.json")
+           .Build();
+        }
+
+        public List<ProductCategory> CreateProductCategory(int numberOfObjects)
+        {
+            List<ProductCategory> listProductCategory = new List<ProductCategory>();
+            Boolean badUserId = true;
+
+            Audit audit = new Audit(1, 130, null, null, false);
+
+
+            for (int i = 1; i <= numberOfObjects; i++)
+            {
+                int createdByUserId;
+                if (badUserId)
+                {
+                    createdByUserId = 999;
+                }
+                else
+                {
+                    createdByUserId = 1;
+                    badUserId = false;
+                }
+
+                String title = Faker.Lorem.Sentence(1);
+
+                ProductCategory prodCat = new ProductCategory(Faker.Lorem.Sentence(1), Faker.Lorem.Sentence(5), 1, false, null, audit);
+                //productCategory.Description = Faker.Lorem.Sentences(5).ToString();
+
+                if (new Random().Next(0, 1) != 0)
+                {
+                    prodCat.Description = Faker.Lorem.Sentence(new Random().Next(1, 5));
+                }
+
+                listProductCategory.Add(prodCat);
+            }
+            return listProductCategory;
+        }
+
+        [TestInitialize]
+        public void PrepareTest()
+        {
+
+            restClient = new RestClient(url);
+
+            //GET Security Token
+            access = GetAccessToken();
+            restRequest = new RestRequest();
+            restRequest.AddHeader("Cache-Control", "no-cache");
+            restRequest.AddHeader("Authorization", "Bearer " + access + "");
+            restRequest.RequestFormat = DataFormat.Json;
+            restRequest.AddHeader("Content-Type", "application/json");
+            restRequest.AddHeader("Accept", "application/json");
+        }
+
+        [TestCleanup]
+        public void TearDownTest()
+        {
+
+        }
+
+
+        private String GetAccessToken()
+        {
+            String accessToken = null;
+            //Get Url for authorisation token from config file
+            string fedUrl = configuration["FedTokenUrl"];
+
+            RestClient client = new RestClient(fedUrl);
+            RestRequest request = new RestRequest(Method.POST);
+            request.AddHeader("Cache-Control", "no-cache");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.RequestFormat = DataFormat.Json;
+            request.AddParameter("client_id", "cascadeDevTest");
+            request.AddParameter("grant_type", "client_credentials");
+            request.AddParameter("client_secret", "yYHX6v4OPl8b");
+            request.AddParameter("scope", scope);
+
+            IRestResponse response = client.Execute(request);
+            APIHelper<AccessTokenResponseDTO> token = new APIHelper<AccessTokenResponseDTO>();
+            AccessTokenResponseDTO content = token.GetContent<AccessTokenResponseDTO>(response);
+
+            if (content.Access_token != null)
+            {
+                accessToken = content.Access_token;
+
+            }
+            return accessToken;
+        }
+
+    }
+}
